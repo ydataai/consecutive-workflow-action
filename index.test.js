2c8d75f63f654e02b57eb444429688abaf3f979c
mktcode <kontakt@markus-kottlaender.de>
2021-07-16 13:49:53 +0200

    removed tests

--- index.test.js
+++
@@ -1,9 +0,0 @@
-const check = require('./check')
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await check()
-  const end = new Date()
-  var delta = Math.abs(end - start)
-  expect(delta).toBeGreaterThanOrEqual(1000)
-})
