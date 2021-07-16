84f13632b7aa7cea19e55a76e24aaee02493be53
mktcode <kontakt@markus-kottlaender.de>
2021-07-16 13:59:36 +0200

    fix bug when there are no previous runs or runs were deleted

--- dist/index.js
+++ dist/index.js
@@ -24,19 +24,23 @@ async function run() {
 
     // fetch the latest workflow runs and find the last one before the currently running one
     const { data: { workflow_runs: runs } } = await octokit.rest.actions.listWorkflowRuns({ owner, repo, workflow_id })
-    let lastRun = runs.find(run => run.run_number === github.context.runNumber - 1)
+    // to take into account that runs can be deleted: sort runs by number and pick the first with a number smaller than the current one
+    let lastRun = runs.sort((a, b) => b.run_number - a.run_number).find(run => run.run_number < github.context.runNumber)
 
     // re-check in intervals, as long as it has not completed
-    while (lastRun.status !== 'completed') {
-      core.info(`Last run (${lastRun.id}) not completed yet. Waiting for ${interval} seconds.`)
-      await sleep(interval)
-      let { data: lastRun } = octokit.rest.actions.getWorkflowRun({
-        owner,
-        repo,
-        run_id: lastRun.id,
-      })
+    if (lastRun) {
+      while (lastRun.status !== 'completed') {
+        core.info(`Last run (${lastRun.id}) not completed yet. Waiting for ${interval} seconds.`)
+        await sleep(interval)
+        let { data: lastRun } = octokit.rest.actions.getWorkflowRun({
+          owner,
+          repo,
+          run_id: lastRun.id,
+        })
+      }
+      core.info(`Last run (${lastRun.id}) has completed.`)
     }
-    core.info(`Last run (${lastRun.id}) has completed.`)
+    core.info(`This is the first time this workflow runs. No checks needed.`)
   } catch (error) {
     core.setFailed(error.message)
   }
