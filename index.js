722bfd793d332d375f74f30e805e0626e622c1ba
mktcode <kontakt@markus-kottlaender.de>
2021-07-16 13:05:29 +0200

    testing

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const check = require('./check')
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    core.info(JSON.stringify(github, null, 2))
+    const token = core.getInput('token')
+    const workflowId = core.getInput('workflow-id')
+    const octokit = github.getOctokit(token)
+    
+    const { data: runs } = await octokit.rest.actions.listWorkflowRuns({
+      owner: github.context.payload.repository.owner.login,
+      repo: github.context.payload.repository.name,
+      workflowId,
+    })
+
+    core.info(JSON.stringify(runs, null, 2))
+
     await check()
   } catch (error) {
     core.setFailed(error.message)
