6cf55ebec6e0daf57d6f52a7d32e5c2c4330a581
mktcode <kontakt@markus-kottlaender.de>
2021-07-16 13:35:59 +0200

    proof of concept

--- check.js
+++
@@ -1,7 +0,0 @@
-const check = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve('done!'), 1000)
-  })
-}
-
-module.exports = check
--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-const check = require('./check')
 
+function sleep(seconds) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
     const token = core.getInput('token')
-    const workflowId = core.getInput('workflow-id')
+    const workflow_id = core.getInput('workflow-id')
+    const interval = core.getInput('interval')
     const octokit = github.getOctokit(token)
 
-    const { data: runs } = await octokit.rest.actions.listWorkflowRuns({
-      owner: github.context.payload.repository.owner.login,
-      repo: github.context.payload.repository.name,
-      workflow_id: workflowId,
-    })
+    const owner = github.context.payload.repository.owner.login
+    const repo = github.context.payload.repository.name
 
-    core.info(JSON.stringify(runs, null, 2))
+    // fetch the latest workflow runs and find the last one before the currently running one
+    const { data: runs } = await octokit.rest.actions.listWorkflowRuns({ owner, repo, workflow_id })
+    let lastRun = runs.find(run => run.run_number === github.context.runNumber - 1)
 
-    await check()
+    // re-check in intervals, as long as it has not completed
+    while (lastRun.status !== 'completed') {
+      core.info(`Last run (${lastRun.id}) not completed yet. Waiting for ${interval} seconds.`)
+      await sleep(interval)
+      let { data: lastRun } = octokit.rest.actions.getWorkflowRun({
+        owner,
+        repo,
+        run_id: lastRun.id,
+      })
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
