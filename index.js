1ade488d6ce74f382c44436a8d81e13ff259959e
mktcode <kontakt@markus-kottlaender.de>
2021-10-15 18:58:06 +0200

    debug

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ async function run() {
     
     
     // re-check in intervals, as long as it has not completed
+    console.log(lastRuns)
     if (lastRuns) {
-      core.info(`Found active workflow runs (${lastRuns.map(obj => obj.id)}).`)
+      core.info(`Found active workflow runs (${JSON.strinfigy(lastRuns.map(obj => obj.id))}).`)
 
       for (let lastRun of lastRuns) {
         while (lastRun.status !== 'completed') {
